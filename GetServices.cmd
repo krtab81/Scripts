7d3bd53c9c53ff7b577c9be8ac1f145676e475b3
krtab81 <cristianriostaboas@hotmail.com>
2024-12-01 11:24:35 +0100

    Add GetServices & GetTasks scripts

---
+++ GetServices.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+	@SETLOCAL
+	@SET DestFile=%1
+	IF "%DestFile%"=="" SET DestFile=%~dp0Services.txt
+	sc queryex  > %DestFile%
+	ECHO Proceso finalizado.
---
+++ GetTasks.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+	@SETLOCAL
+	@SET DestFile=%1
+	IF "%DestFile%"=="" SET DestFile=%~dp0Tasks.txt
+	schtasks /Query /V > %DestFile%
+	ECHO Proceso finalizado.
